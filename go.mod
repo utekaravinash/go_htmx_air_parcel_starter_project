3b11ca3fbb91608c5571d959a7057476c62d15e6
Avinash Utekar <utekaravinash@gmail.com>
2023-11-11 19:29:55 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/utekaravinash/go_htmx_air_parcel_starter_project
+
+go 1.21.4
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
